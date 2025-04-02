69f04cad5981a66967664794b069cd33f73c2a90
Mattia-FR <mattia.bouvier@protonmail.com>
2025-04-02 15:32:06 +0200

    C:/Program Files/Git/api/ninjas on progress

--- src/router/index.js
+++ src/router/index.js
@@ -4,4 +4,13 @@ const router = express.Router();
 const ninjasRouter = require("./ninjasRouter");
 router.use("/ninjas", ninjasRouter);
 
+// const villagesRouter = require("./villagesRouter");
+// router.use("/villages", villagesRouter);
+
+// const jutsusRouter = require("./jutsusRouter");
+// router.use("/jutsus", jutsusRouter);
+
+// const missionsRouter = require("./missionsRouter");
+// router.use("/missions", missionsRouter);
+
 module.exports = router;
--- src/router/ninjasRouter.js
+++ src/router/ninjasRouter.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const db = require("../model/db");
 
 const router = express.Router();
 
 // get http://localhost:4242/api/ninjas/
-router.get("/", (req, res) => {
-	console.log("Je suis sur la route /api/ninjas/");
+router.get("/", async (req, res) => {
+	const [ninjas] = await db.query("select * from ninjas");
+	res.json(ninjas);
 });
 
 module.exports = router;
